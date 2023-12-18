9cabdc318d250762c9dfea747f967634ff634e1c
BinaryROK <wlstltntn@gmail.com>
2023-12-18 23:00:53 +0900

    predict try2

--- two/views.py
+++ two/views.py
@@ -24,16 +24,28 @@ def two(request, selected_date='default_date_value'):
 
 # 기존의 chart_data_2 함수 수정
 def chart_data_2(request, selected_date):
-    file_path = f'Data/zaraData/{selected_date}.csv'
+    actual_file_path = f'Data/zaraData/{selected_date}.csv'
+    predicted_file_path = f'Data/predicted/{selected_date}.csv'
 
-    df = read_csv_file(file_path)
+    actual_df = read_csv_file(actual_file_path)
+    predicted_df = read_csv_file(predicted_file_path)
 
-    # 시간과 예측값을 가져와서 리스트로 변환
-    labels = df['time'].tolist()
-    data = df['amount'].tolist()
+    # 실제 및 예측 값에 대한 시간과 양 데이터 추출
+    actual_labels = actual_df['time'].tolist()
+    actual_data = actual_df['amount'].tolist()
+
+    predicted_labels = predicted_df['time'].tolist()
+    predicted_data = predicted_df['amount'].tolist()
+
     chart_data = {
-        'labels': labels,
-        'data': data,
+        'labels': {
+            'actual': actual_labels,
+            'predicted': predicted_labels,
+        },
+        'data': {
+            'actual': actual_data,
+            'predicted': predicted_data,
+        },
     }
 
     return JsonResponse(chart_data)
