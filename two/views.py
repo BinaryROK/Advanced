0196fc19a31a7237e836719992e24f0684147604
BinaryROK <wlstltntn@gmail.com>
2023-12-19 03:03:38 +0900

    fk

--- two/views.py
+++ two/views.py
@@ -5,30 +5,29 @@ from django.http import JsonResponse
 import pandas as pd
 from django.http import HttpResponse
 
-import csv
-
-# two/views.py에 read_csv_file 함수 정의 추가
+# 함수를 정의하여 CSV 파일 읽기
 def read_csv_file(file_path):
     df = pd.read_csv(file_path)
     return df
 
-# two/views.py
 def two(request, selected_date='default_date_value'):
     if selected_date == 'default_date_value':
-        # 디폴트 값에 해당하는 처리
         return HttpResponse("Please select a date.")
 
-    file_path = f'Data/zaraData/{selected_date}.csv'
-    return render(request, 'two/two.html', {'file_path': file_path, 'selected_date': selected_date})
+    zara_file_path = f'Data/zaraData/{selected_date}.csv'
+    predicted_file_path = f'Data/predicted/{selected_date}.csv'
 
+    return render(request, 'two/two.html', {
+        'zara_file_path': zara_file_path,
+        'predicted_file_path': predicted_file_path,
+        'selected_date': selected_date
+    })
 
-# 기존의 chart_data_2 함수 수정
 def chart_data_2(request, selected_date):
-    file_path = f'Data/predicted/{selected_date}.csv'
+    file_path = f'Data/zaraData/{selected_date}.csv'
 
     df = read_csv_file(file_path)
 
-    # 시간과 예측값을 가져와서 리스트로 변환
     labels = df['time'].tolist()
     data = df['amount'].tolist()
     chart_data = {
@@ -38,4 +37,16 @@ def chart_data_2(request, selected_date):
 
     return JsonResponse(chart_data)
 
+def new_chart_data(request, selected_date):
+    file_path = f'Data/predicted/{selected_date}.csv'
 
+    df = read_csv_file(file_path)
+
+    labels = df['time'].tolist()
+    data = df['amount'].tolist()
+    chart_data = {
+        'labels': labels,
+        'data': data,
+    }
+
+    return JsonResponse(chart_data)
